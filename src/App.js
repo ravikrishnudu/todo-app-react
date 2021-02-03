151f17a2e2ba6936a892065fbddb406f27d5e959
ravikrishnudu <52109411+ravikrishnudu@users.noreply.github.com>
2021-02-03 12:18:44 +0530

    setup
    setuo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React,{ usestate } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>heghgllo  Ravi</h1>
+     <input />
+     <button>Add Todo</button>
+     <ul>
+      <li>Take dogs for walk</li>
+      <li>Take the rubbish</li>
+     </ul>
     </div>
   );
 }
